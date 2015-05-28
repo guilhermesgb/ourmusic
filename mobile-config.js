9a1eaf4b660d71fba34369603ecefe702bf1277d
Guilherme Baptista <guisgb13@gmail.com>
2015-05-28 03:16:27 +0200

    Initial file

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+App.info({
+    id: 'com.ourmusic.ourmusic',
+    name: 'OurMusic',
+    description: '',
+    author: 'Guilherme Xibaca, Danilo Pimentxenha and Uian Sol',
+    email: 'guisgb13@gmail.com',
+    website: 'https://github.com/guilhermesgb/ourmusic'
+});
+
+App.configurePlugin('com.ourmusic.plugin', {});
