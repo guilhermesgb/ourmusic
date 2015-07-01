31673defcab7f83c211d00197ca9fb3743152ff1
Guilherme Baptista <guisgb13@gmail.com>
2015-07-01 21:28:37 +0200

    Subscribing to userData in the playRoom controller

--- client/controllers/playRoom.js
+++ client/controllers/playRoom.js
@@ -1,38 +1,40 @@
 angular.module("ourmusic").controller("PlayRoomCtrl", [
     '$scope', '$stateParams', '$meteor',
     function($scope, $stateParams, $meteor) {
-        $meteor.subscribe("play_rooms").then(function(subscription) {
-            $scope.playRoom = $meteor.object(PlayRooms, {
-                'roomId': 'GLOBAL'
-            });
+        $meteor.subscribe("userData").then(function(subscription) {
+            $meteor.subscribe("play_rooms").then(function(subscription) {
+                $scope.playRoom = $meteor.object(PlayRooms, {
+                    'roomId': 'GLOBAL'
+                });
 
-            $scope.startOrResumePlayer = function() {
-                $meteor.call("play", $scope.playRoom.roomId).then(
-                    function() {
-                        console.log("Play request placed successfully.");
-                    }, function(error) {
-                        alert(error);
-                    }
-                );
-            }
-
-            $scope.pausePlayer = function() {
-                $meteor.call("pause", $scope.playRoom.roomId).then(
-                    function() {
-                        console.log("Pause request placed successfully.");
-                    }, function(error) {
-                        alert(error);
-                    }
-                );
-            }
-
-            $scope.sendOperationToPlayer = function() {
-                if ($scope.playRoom.playerState.playing === true) {
-                    $scope.pausePlayer();
-                } else {
-                    $scope.startOrResumePlayer();
+                $scope.startOrResumePlayer = function() {
+                    $meteor.call("play", $scope.playRoom.roomId).then(
+                        function() {
+                            console.log("Play request placed successfully.");
+                        }, function(error) {
+                            alert(error);
+                        }
+                    );
                 }
-            }
+
+                $scope.pausePlayer = function() {
+                    $meteor.call("pause", $scope.playRoom.roomId).then(
+                        function() {
+                            console.log("Pause request placed successfully.");
+                        }, function(error) {
+                            alert(error);
+                        }
+                    );
+                }
+
+                $scope.sendOperationToPlayer = function() {
+                    if ($scope.playRoom.playerState.playing === true) {
+                        $scope.pausePlayer();
+                    } else {
+                        $scope.startOrResumePlayer();
+                    }
+                }
+            });
         });
     }
 ]);
